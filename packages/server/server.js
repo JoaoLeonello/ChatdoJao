15d21cb87be3cb727bd0dd7927ab6148a48d8b8d
jfelipe.pl@gmail.com <joao.felipe@wdna.com.br>
2021-05-24 13:07:43 -0300

    Socket config improve

--- packages/server/server.js
+++ packages/server/server.js
@@ -5,14 +5,21 @@ const server = require("http").createServer(app);
 const SocketIO = require("socket.io");
 io = SocketIO(server, { cors: { origin: '*' }, pingInterval: 60000 });
 
-
-io.engine.on('connection', async (socket) => {
-    console.log('connection', socket.id)
+let socketsConnected = []
+io.on('connection', async (socket) => {
+    socketsConnected.push(socket.id);
+    socket.emit('socketsConnected', { socketsConnected });
 
     socket.on('client_emit_message', (messageObject) => {
-        console.log('here')
+        io.emit('messageChannel', messageObject);
         console.log(messageObject)
     });
 });
+
+io.on('disconnect', async (socket) => {
+    console.log('DISCONNECTED')
+    socketsConnected = socketsConnected.filter(sckt => sckt.id !== socket.id)
+    socket.emit('socketsConnected', { socketsConnected });
+})
 
 server.listen(3052, () => { console.log('Server ONLINE') });
