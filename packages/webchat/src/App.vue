c55b5d91c13e3ce9cf1c23f7762797c4e573de0f
JoaoLeonello <jfelipe.pl@gmail.com>
2021-07-22 22:55:45 -0300

    Fix

--- packages/webchat/src/App.vue
+++ packages/webchat/src/App.vue
@@ -27,13 +27,6 @@
           <div
             class="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg"
           >
-            <!-- <div class="h-20 w-20 rounded-full border overflow-hidden">
-              <img
-                src="https://www.facebook.com/photo?fbid=4257212934307787&set=a.160204470675341"
-                alt="Avatar"
-                class="h-full w-full"
-              />
-            </div> -->
             <input
               placeholder="Username"
               type="text"
